13de2b93030d188430e1c0fff4c51743d578e189
Joncygeorge <121477511+Joncygeorge@users.noreply.github.com>
2024-11-14 19:32:52 +0000

    update trains

--- script.js
+++ script.js
@@ -6,4 +6,23 @@ const trainName= document.getElementById("trainName").value
 const trainNumber= document.getElementById("trainNumber").value
 const source= document.getElementById("source").value
 const destination= document.getElementById("destination").value
+
+if (trainId) {
+    // update train
+    const index = trains.findIndex(train => train.id == trainId);
+    trains[index] = { id: trainId, trainName, trainNumber, source, destination };
+} else {
+    // Add new train
+    const newTrain = {
+        id: Date.now().toString(),
+        trainName,
+        trainNumber,
+        source,
+        destination
+    };
+    trains.push(newTrain);
+}
+
+document.getElementById("trainForm").reset();
+renderTrainList();
 }
