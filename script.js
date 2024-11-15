c076a62fb19f57db4e7e0916fc690861cf8f3ce9
Joncy George <joncygeorge25@gmail.com>
2024-11-15 08:58:44 +0000

    update changes in script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let trains =[];
+function saveTrain(event){
+    event.preventDefault();
+    const trainId = document.getElementById("trainId").value
+    const trainName = document.getElementById("trainName").value
+    const trainNumber = document.getElementById("trainNumber").value
+    const source = document.getElementById("source").source
+    const destination = document.getElementById("destination").value
+}
+
+
+
